46852a48b39701708d0250ce1791b8231834d13f
EBosi <bosiemanuele@gmail.com>
2025-02-27 11:06:15 +0100

    Spostato ampwrap.py in ampwrap/cli.py e aggiornato setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "ampwrap=ampwrap.cli:main",  # Esegue ampwrap/cli.py come CLI
+        ],
+    },
 )
