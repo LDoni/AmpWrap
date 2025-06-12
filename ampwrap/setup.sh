5fe76aaf931c699ccb988f2fc6af28444245b4df
Lapo Doni <48062644+LDoni@users.noreply.github.com>
2025-06-12 10:06:58 +0200

    Update setup.sh

--- ampwrap/setup.sh
+++ ampwrap/setup.sh
@@ -1,5 +1,5 @@
 for i in AmpWrap_long  AmpWrap_short  ampwrap  scripts  snakefile.long  snakefile.short
 do
-	cp -r ampwrap/$i $CONDA_PREFIX/bin
+	cp -r $i $CONDA_PREFIX/bin
 	chmod -R +x $CONDA_PREFIX/bin/$i
 done
